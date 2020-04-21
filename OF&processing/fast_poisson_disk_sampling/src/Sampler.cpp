39b28e2b07b19b87c836cbe8fc48fdb946254d3b
waihinchan <52313420+waihinchan@users.noreply.github.com>
2020-04-21 21:01:01 +0100

    增加了一个图片采样的效果
    以后可以考虑使用enmu
    主要是2个模式 一个是基于r的模式
    另外一个是基于宽高的模式
    毕竟这是采样器 得有一个采样的区域
    其实同时也可以让图片resize 但是不是很会用那个功能

--- OF&processing/fast_poisson_disk_sampling/src/Sampler.cpp
+++ OF&processing/fast_poisson_disk_sampling/src/Sampler.cpp
@@ -11,55 +11,46 @@
 //删掉与原有的点相隔的太近的点
 //将剩下的点加入输出数组，处理数组
 //如果处理数组中还有点，继续第3步，否则输出数组就是结果。
-//
-//————————————————
-//版权声明：本文为CSDN博主「哗啦cl」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-//原文链接：https://blog.csdn.net/qq_43439240/article/details/104328586
+
 #include "Sampler.hpp"
-//#include "Math.h"
+
 void Sampler::init(int _r, int _k, int _dimension){
-    
     r = _r;
     k = _k;
     dimension = _dimension;
-    w =  r/sqrt(dimension);
+    w =  floor(r/sqrt(dimension));
     //2*(w/2)^2 = r^2
     cols = floor(ofGetHeight()/w);
     rows = floor(ofGetWidth()/w);
+    std::cout<<"w= "<< w <<endl;
+    std::cout<<"cols= "<< cols <<endl;
+    std::cout<<"rows= "<< rows <<endl;
     grid = std::unique_ptr<ofVec2f[]>(new ofVec2f[cols*rows]{ofVec2f(-1, -1)});
-    
-    //用指针的缺点就是如果我要数组长度还是很麻烦 如果你不知道的话你的逻辑的话很难搞出来 这个有点像array练习（说白了C++做这行还是不太适合的感觉）
-    //step1 随便初始化一个点
+    std::cout<<"succeed init! "<<"grid_amount "<< cols*rows <<endl;
     ofVec2f init_random_pos =  ofVec2f(ofRandomWidth(), ofRandomHeight());
-    //这里还有一个问题是 如果我是采样像素的话 我要把这个改成floor 或者怎么样让他能够一一对应像素呢 感觉这里算法要再去细化
     active.push_back(init_random_pos);
     int i = floor(init_random_pos.x/w);
     int j = floor(init_random_pos.y/w);
-    grid[i*rows+j] = init_random_pos;
-    //    j*width+i
-    std::cout<<"succeed init! "<<"grid_amount "<< cols*rows <<endl;
-    std::cout<<"init point"<<init_random_pos<<"index in grid "<<i*rows+j<<endl;
+    grid[j*rows+i] = init_random_pos;
+    
+
+
+    std::cout<<"init point "<<init_random_pos<<" index in grid "<<i*rows+j<<endl;
 
 }
 
 void Sampler::sampling(){
-    if(!active.empty()){
-        for(int ii = 0; ii<25;ii++){//这个决定一次画多少个点 可要可不要 记得上下中括号扩掉
-    //        step1:生成一个随机点
-            if(!active.empty()){
+    while(!active.empty()){
             int random_active_index = floor(ofRandom(active.size()));
-            std::cout<<random_active_index<<"size"<<active.size()<<endl;
+
             ofVec2f sampling_pos = active[random_active_index];
-                //这句有点问题 看看是不是随机的问题
-    //        step2:以这个点为圆心 寻找k次 外环（r到2r的点）
+            std::cout<<random_active_index<<" in size "<<active.size()<<endl;
             bool found = false;
             for(int search = 0; search<k;search++){
                 ofVec2f next_candicate_sample = search_candicate(sampling_pos)+sampling_pos;
-                //这里我们一共获取k次 以samlingpos为中心 距离为r到2r的随机点
-                //下一步计算距离 把这个点丢进去就可以了。 直接返回true或者false
                 
-                //如果找到match 就把这个点放到grid和active
                 if(caculate_distance(next_candicate_sample)){
+                    std::cout<<" push "<<next_candicate_sample<<endl;
                     active.push_back(next_candicate_sample);
                     samples.push_back(next_candicate_sample);
                     int x = floor(next_candicate_sample.x/w);
@@ -67,47 +58,40 @@ void Sampler::sampling(){
                     grid[y*rows+x] = next_candicate_sample;
                     found = true;
                 }
+                
             }
+            
             if(!found){
                 active.erase(active.begin()+random_active_index-1);
                 std::cout<<"delete"<<random_active_index<<endl;
             }
-        //把这个删除掉。
-            }
-        }
     }
-    }
+}
+
 
 
 
 ofVec2f Sampler::search_candicate(ofVec2f _sampling_pos){
     float random_R = ofRandom(r, 2*r);
-    float random_direction = ofRandom(TWO_PI);
+    float random_direction = ofRandom(0,TWO_PI);
     return ofVec2f(cos(random_direction)*random_R,sin(random_direction)*random_R);
+
 }
 bool Sampler::caculate_distance(ofVec2f _next_candicate_sample){
     
-//    if(_next_candicate_sample)
     int x = floor(_next_candicate_sample.x/w);
     int y = floor(_next_candicate_sample.y/w);
-    //这里先把候选点映射到w范围内
+
     bool match = false;
-    if(x>=-1&&y>=-1&&x<=rows-1&&y<=cols-1){//这里检查是否越界 即不能出框 step1
+    if(x>=1&&y>=1&&x<=rows-1&&y<=cols-1){
         if(grid[y*rows+x].x!=-1&&grid[y*rows+x].y!=-1){
-            //这里检查他自己是否为空
             match = true;
-            //match要写在这里 因为要match首先要符合你占据的grid是没有东西且没有越界的
             for(int i = -1;i<1;i++){
                 for(int j = -1;j<1;j++){
-                    //这个循环嵌套检查周围的8个点
-                    
                     int index = (j+y)*rows+i+x;
-                    
                     if(grid[index].x!=-1&&grid[index].y!=-1){
-                           //这里检查他的邻居是否为空 如果不为空 即可以检查边界
                         if(grid[index].distance(_next_candicate_sample)<r){
                             match = false;
-                            //只要有一个就不符合了
                             break;
                         }
                     }
@@ -115,14 +99,11 @@ bool Sampler::caculate_distance(ofVec2f _next_candicate_sample){
             }
         }
     }
-    
-    //基本上这里的循环就是如果一开始的点周围都是没有邻居的 所以基本上虽然到检查邻居是否为空那一步就结束了
     return match;
 }
 
 void Sampler::visualization(){
     ofSetColor(255);
-//    ofSetLineWidth(1);
     for(int i = 0 ; i < samples.size();i++){
         ofDrawCircle(samples[i].x,samples[i].y,1);
     }
--- OF&processing/fast_poisson_disk_sampling/src/ofApp.cpp
+++ OF&processing/fast_poisson_disk_sampling/src/ofApp.cpp
@@ -1,28 +1,31 @@
 #include "ofApp.h"
 #include "Sampler.hpp"
 Sampler fast_poission_disk_sampler;
-std::unique_ptr<ofVec2f[]> active;
-//ofVec2f *active;
-//ofVec2f orginalarray [3] = {ofVec2f(-1, -1),ofVec2f(-2, -1),ofVec2f(-3, -1)};
-//测试智能指针
 
+//ofVec2f orginalarray [3] = {ofVec2f(-1, -1),ofVec2f(-2, -1),ofVec2f(-3, -1)};
+
+int scl;
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    ofBackground(0);
-    fast_poission_disk_sampler.init();
-
-    
-    
 //--------------------------------------------------------------测试智能指针
 //    active = std::unique_ptr<ofVec2f[]>(new ofVec2f[3]{ofVec2f(-1, -1),ofVec2f(-2, -1),ofVec2f(-3, -1)});
 //    active = new ofVec2f[3]{ofVec2f(-1, -1),ofVec2f(-2, -1),ofVec2f(-3, -1)};
 //    active = orginalarray;
 //--------------------------------------------------------------
 
+    ofBackground(0);
+    fast_poission_disk_sampler.init();
+    fast_poission_disk_sampler.sampling();
+    myImage.load("bbb.jpg");
+    myImage.allocate(myImage.getWidth(), myImage.getHeight(), OF_IMAGE_COLOR);
+    ofLoadImage(pxdata, "bbb.jpg");
     
-    
-    
+    for(int i = 0;i<fast_poission_disk_sampler.samples.size();i++){
+        std::cout<<fast_poission_disk_sampler.samples[i]<<endl;
+    }
+//    std::cout<<fast_poission_disk_sampler.samples.size()<<endl;
+//    std::cout<<pxdata.size()<<endl;
 }
 
 //--------------------------------------------------------------
@@ -37,9 +40,33 @@ void ofApp::draw(){
 //        std::cout<<active[i].getNormalized() <<endl;
 //    }
 //--------------------------------------------------------------
-    fast_poission_disk_sampler.sampling();
-    fast_poission_disk_sampler.visualization();
+//    fast_poission_disk_sampler.sampling();
+//    fast_poission_disk_sampler.visualization();
+//--------------------------------------------------------------
     
+
+    
+    
+    
+    for(int i = 0;i<fast_poission_disk_sampler.samples.size();i++){
+        
+        float xscl = myImage.getHeight()/ofGetWidth();
+        float yscl = myImage.getHeight()/ofGetHeight();
+        float orginalxpos =floor(fast_poission_disk_sampler.samples[i].x);
+        float orginalypos =floor(fast_poission_disk_sampler.samples[i].y);
+
+        int xpos = floor(orginalxpos*xscl);
+        int ypos = floor(orginalypos*yscl);
+        int index = ypos*myImage.getWidth()+xpos;
+
+        int r = pxdata.getData()[index*3+0];
+        int g = pxdata.getData()[index*3+1];
+        int b = pxdata.getData()[index*3+2];
+
+        ofSetColor(r, g, b);
+        ofDrawCircle(orginalxpos, orginalypos, 3);
+
+    }
     
     
     
